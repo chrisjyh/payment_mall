66043624217f70d7c1078fb95723a4494b4a174a
jooeunho <pureredgrace@gmail.com>
2023-08-31 19:00:11 +0900

    장바구니 수정기능 추가

--- mall/views.py
+++ mall/views.py
@@ -7,7 +7,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from django.contrib import messages
 from django.shortcuts import redirect
-
+from django.forms import modelformset_factory
+from .forms import CartProductForm
 # Create your views here.
 # def product_list(request):
 #     # select_related 하면 category과 관련된것을 조회해서 데이터를 불러줌
@@ -40,8 +41,50 @@ class ProductListView(ListView):
 product_list = ProductListView.as_view()
 
 @login_required
+def cart_detail(request):
+    # select_related를 안하게 되면 조회를 여러번하기때문에 조인을 해서 쿼리조회를 최소화
+    cart_product_qs = (CartProduct.objects.filter(
+        user=request.user,
+    )
+    .select_related("product")
+    # 이름기준으로 오름차순 
+    .order_by("product__name")                  
+    )
+    
+    # 장바구니의 수정 폼셋 설정
+    CartProductFormSet = modelformset_factory(
+        model=CartProduct,
+        form=CartProductForm,
+        # 추가적인 줄 필요 x
+        extra=0,
+        # 삭제 가능
+        can_delete=True, 
+    )
+    if request.method == "POST":
+        formset = CartProductFormSet(
+            data=request.POST,
+            # 상단에 정의된 쿼리 가져옴
+            queryset=cart_product_qs,  
+        )
+        if formset.is_valid():
+            formset.save()
+            messages.success(request, "장바구니가 수정되었습니다.")
+            return redirect("cart_detail")
+    else:
+        formset = CartProductFormSet(
+            queryset=cart_product_qs,
+        )
+    
+    
+    return render(request, "mall/cart_detail.html",{
+        "formset": formset,
+    })
+
+@login_required
+# 장바구니에 상품 추가 하는 기능
 def add_to_cart(request, product_pk):
     request.user
+    # 상품상태가 ACTIVE인 상태를 조회
     product_qs = Product.objects.filter(
         status=Product.Status.ACTIVE
     )
@@ -53,10 +96,14 @@ def add_to_cart(request, product_pk):
         product=product,
         defaults={"quantity": quantity},
     )
+    
     if not is_created:
         cart_product.quantity += quantity
         cart_product.save()
     
     messages.success(request, "장바구니에 추가했습니다.")
     
-    return redirect("product_list")
+    # 장바구니 추가될시 페이지가 초기화면으로 이동하는 문제
+    redirect_url = request.META.get("HTTP_REFERER", "product_list")
+    
+    return redirect(redirect_url)
